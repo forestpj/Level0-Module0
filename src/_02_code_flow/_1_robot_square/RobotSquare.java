fb5e1c85cbbfd55b40f94da7ac14442613366e3d
root <root@54f6353a7182>
2020-08-18 01:28:21 +0000

    day three of coding

--- src/_02_code_flow/_1_robot_square/RobotSquare.java
+++ src/_02_code_flow/_1_robot_square/RobotSquare.java
@@ -13,31 +13,55 @@ public class RobotSquare {
 	public static void main(String[] args) {
 
 		// This code makes a new Robot
-		Robot rob = new Robot();
+		Robot octogon = new Robot();
 
 		// Put the robot's pen down so it can draw
 		// Use the rob.penDown(); command.
-		rob.penDown();
+		octogon.penDown();
 		// SPEED. Make the robot move quickly.
 		// Use the rob.setSpeed(); command.
-		rob.setSpeed(100);
-		
+		octogon.setSpeed(100);
+
 		for (int i = 0; i < 10; i++) {
 			// DRAW. Draw a line
 			// Use the rob.move(); command
-			rob.move(10);
+			octogon.move(100);
 
 			// TURN. Turn 90 degrees
 			// Use the rob.turn(); command
-			rob.turn(90);
-			
-			rob.move(10);
-			
-			rob.turn(270);
-			
-			// LOOP. Now put the DRAW and TURN code into a for loop to repeat 4 times.
-			// Did your Robot draw a square?
+			octogon.turn(45);
 		}
 
+		Robot square = new Robot();
+
+		square.moveTo(450, 370);
+
+		square.penDown();
+
+		square.setSpeed(100);
+
+		for (int i = 0; i < 10; i++) {
+
+			square.move(241);
+
+			square.turn(90);
+		}
+
+		// LOOP. Now put the DRAW and TURN code into a for loop to repeat 4 times.
+		// Did your Robot draw a square?
+		Robot star = new Robot();
+
+		star.moveTo(522, 370);
+
+		star.penDown();
+
+		star.setSpeed(100);
+
+		for (int i = 0; i < 10; i++) {
+
+			star.move(240);
+			
+			star.turn(135);
+		}
 	}
 }
--- src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
+++ src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
@@ -12,23 +12,23 @@ public class RobotSpiral {
 	public static void main(String[] args) {
 		
 		// Create a new Robot
-		
+		Robot idk = new Robot();
 		// Set your robot's pen down 
-		
+		idk.penDown();
 		// SPEED. Set the robot to go at max speed (100)
-		
+		idk.setSpeed(1000000000);
         // LOOP. Use a for loop to repeat ALL the following lines of code 50 times. 
-		
+		for (int i = 0; i < 1000000000; i++) {
 				// Change the robot pen color to random
-			
+		idk.setRandomPenColor();
 				// DRAW.    Move the robot (5*i) pixels
 				//          "i" is the variable in the for loop	
-			
+		idk.move(2*i);
 				// TURN.    Turn the robot (360/7) degrees to the right
-			
+		idk.turn(720/5);
 				// Change the robot pen width to (i)
-
-		
+        idk.setPenWidth(i);
+		}
 		// Check the pattern against the picture in the recipe. If it matches, you are done!
 		
 	}
