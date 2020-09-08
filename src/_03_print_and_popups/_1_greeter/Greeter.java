f9451889bec8fa726c2012862420b8c2e65a98d1
root <root@8d5bffe2c3df>
2020-09-08 01:27:00 +0000

    day 5 of coding

---
+++ src/_03_print_and_popups/_1_greeter/Greeter.java
@@ -0,0 +1,17 @@
+package _03_print_and_popups._1_greeter;
+
+import javax.swing.JOptionPane;
+
+public class Greeter {
+public static void main(String[] args) {
+	
+System.out.println("hello world!");
+
+String input = JOptionPane.showInputDialog("What is your name?");
+
+JOptionPane.showMessageDialog(null,"Hello "+input+"! Have a great day.");
+
+
+
+}
+}
--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,11 +8,15 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String input = JOptionPane.showInputDialog(null, "Do you know how to write code?");
 		// 2. If they say "yes", tell them they will rule the world.
-
+		if(input.equals("yes")) {
+			JOptionPane.showMessageDialog(null,"With this skill you will rule the world!");
+		}
 		// 3. Otherwise, tell them to sign up for classes at The League.
-
+		else {
+			JOptionPane.showMessageDialog(null,"Well you should sign up at The League so you can answer yes to this question later on.   UwU");
+		}
 	}
 }
 
