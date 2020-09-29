b601411e1773f73fa2003044b81bca1603184c26
root <root@cd78aed8941a>
2020-09-29 01:27:29 +0000

    day 9 of coding

--- src/_04_amazing_games/TerminatorCat/TerminatorCat.pde
+++ src/_04_amazing_games/TerminatorCat/TerminatorCat.pde
@@ -17,14 +17,14 @@ void setup() {
 // 2. The code below loads your cat picture into the program. 
 //     Make sure the file name is correct for the cat image you saved earlier
 cat = loadImage("cat.jpg");
-  
+  size(500, 500);
 // 3. Set the size of the sketch. Make it big enough to show the cat you chose.
-
+  size(500, 500);
 // 4. Resize the cat so it is the same size as the sketch
-
+  cat.resize(500,500);
 // 5. DRAW CAT.    Use the background() command to make the cat the background of the sketch
 //    Run the program to see if the cat is drawn. Get this working before you go on.
- 
+  background(cat);
 
   }
 
--- src/_04_amazing_games/_2_dragon_fight/DragonFight.java
+++ src/_04_amazing_games/_2_dragon_fight/DragonFight.java
@@ -35,9 +35,9 @@ public class DragonFight {
 
 				// 3. Ask the player in a pop-up if they want to attack the dragon with a yell
 				// or a kick
-			String input = JOptionPane.showInputDialog(null, "Would you like to yell or kick at the dragon?");{
+			String input = JOptionPane.showInputDialog(null, "do you want to yell or kick at the dragon?");
 				// 4. If they typed in "yell":
-//			if(input.equals("yell")) {
+		if(input.equals("yell")) {
 					// -- Find a random number between 0 and 10 and store it in playerAttack. Use
 					// ran.nextInt(10)
 				playerattack = ran.nextInt(10);
@@ -47,51 +47,60 @@ public class DragonFight {
 				// 5. If they typed in "kick":
 				if(input.equals("kick")) {
 					// -- Find a random number between 0 and 25 and store it in playerAttack.
-				ran.nextInt(25);
+				playerattack=ran.nextInt(25);
 					// -- Subtract the player attack value from the dragon's health
-				playerattack = ran.nextInt(25);
 				
 				dragonhealth-=playerattack;
 		}
 			// THE DRAGON RETALIATES
 
 				// 6. Find a random number between 0 and 35 and store it in dragonAttack
-				ran.nextInt(35);
+				dragonattack=ran.nextInt(35);
 				// 7. Subtract the dragon attack value from the player's health
 				playerhealth-=dragonattack;
 			// ASSESS THE DAMAGE CAUSED 
 				
 				// 8. If the player's health is less than or equal to 0
 				// call the playerLost() method
-				if (playerhealth == 0);
-				
-				JOptionPane.showMessageDialog(null, "You lost.");
+				if (playerhealth <= 0) {
+					playerLost();
+				}
 				// 9. Else if the dragon's health is less than or equal to 0
 				// call the dragonLost() method
-
+				else if (dragonhealth <= 0){
+					dragonLost();
+					
+				}
 			
 			// 10.  Pop up a message that tells the player the health levels of player and
 			// 		dragon.
-
-			
+				JOptionPane.showMessageDialog(null, "you have done " + playerattack + " damage to the dragon");
+				
+				JOptionPane.showMessageDialog(null, "the dragon now has " + dragonhealth + " health");
+				
+				JOptionPane.showMessageDialog(null, "the dragon has attacked! you've taken " + dragonattack + " health");
+				
+				JOptionPane.showMessageDialog(null, "you now have "+ playerhealth + " health");
+				
 			// (Bonus: Also display the amount of health that was lost by each in this
 			// round)
-
 		}
-
 	}
 
 	static void playerLost() {
-		// 11. Tell the player that they have been defeated by the dragon and have no treasure
+		// 11. Tell the player that they have been defeated by the dragon and have no
+		// treasure
 
-
-		System.exit(0);   //This code ends the program
+		JOptionPane.showMessageDialog(null, "You have been defeated by the dragon and have no treasure");
+		
+		System.exit(0); // This code ends the program
 	}
 
 	static void dragonLost() {
-		// 12. Tell the user that the dragon has been defeated and they get a ton of gold!
-
-		System.exit(0);   //This code ends the program
+		// 12. Tell the user that the dragon has been defeated and they get a ton of
+		// gold!
+		JOptionPane.showMessageDialog(null, "you have defeted the dragon! You now have a ton of treasure!");
+		System.exit(0); // This code ends the program
 	}
 
 }
